cdd93a0edddac8e45b661f7ed4611db58370d2d9
Daisy Zhou <daisy@wibidata.com>
2013-01-31 10:31:06 -0800

    SCHEMA-109. Update to reflect API change in KijiInstaller. over-the-shoulder review: Juliet
    commit: https://github.com/kijiproject/kiji-schema-shell/commit/e2d1917da7735f06edade28e4a97a60df60c7b55

--- kiji-schema-shell/src/test/scala/org/kiji/schema/shell/IntegrationTestKijiSystem.scala
+++ kiji-schema-shell/src/test/scala/org/kiji/schema/shell/IntegrationTestKijiSystem.scala
@@ -213,7 +213,7 @@ WITH LOCALITY GROUP default WITH DESCRIPTION 'main storage' (
    */
   def installKiji(instanceName: String): Unit = {
     val kijiUri = KijiURI.newBuilder("kiji://.env/" + instanceName).build()
-    KijiInstaller.install(kijiUri, HBaseConfiguration.create())
+    KijiInstaller.get().install(kijiUri, HBaseConfiguration.create())
   }
 
   /**
