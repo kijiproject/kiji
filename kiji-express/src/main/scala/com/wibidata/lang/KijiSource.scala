dbf3734b99c6ad4288f846f335dcdb670e615eb9
Robert Chu <robert@wibidata.com>
2013-03-04 10:44:24 -0800

    CHOP-2. Project uses a Maven build.
    This commit modifies Chopsticks so that the project uses a Maven
    build. A few clean-up changes are also made in KijiSource.scala.
    
    Signed-off-by: Kiyan Ahmadizadeh <kiyan@wibidata.com>
    commit: https://github.com/kijiproject/kiji-express/commit/79623f3bd4a18cdd1ffaf2ddc264a882f12fd4c9

--- kiji-express/src/main/scala/com/wibidata/lang/KijiSource.scala
+++ kiji-express/src/main/scala/com/wibidata/lang/KijiSource.scala
@@ -1,5 +1,6 @@
 package com.wibidata.lang
 
+import cascading.scheme.Scheme
 import cascading.tap.Tap
 import com.twitter.scalding.AccessMode
 import com.twitter.scalding.Hdfs
@@ -7,6 +8,9 @@ import com.twitter.scalding.Mode
 import com.twitter.scalding.Read
 import com.twitter.scalding.Source
 import com.twitter.scalding.Write
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapred.RecordReader
+import org.apache.hadoop.mapred.OutputCollector
 
 import org.kiji.schema.KijiDataRequest
 import org.kiji.schema.KijiURI
@@ -15,14 +19,14 @@ case class KijiSource(
     dataRequest: KijiDataRequest,
     tableURI: KijiURI) extends Source {
 
-  // override val hdfsScheme: Scheme[JobConf, RecordReader[_, _], OutputCollector[_, _], _, _] =
   override def hdfsScheme = new KijiScheme(dataRequest)
+      .asInstanceOf[Scheme[JobConf, RecordReader[_, _], OutputCollector[_, _], _, _]]
 
   override def createTap(readOrWrite: AccessMode)(implicit mode: Mode): Tap[_, _, _] = {
     val kijiScheme = hdfsScheme.asInstanceOf[KijiScheme]
     val tap: Tap[_, _, _] = mode match {
       case Hdfs(_, _) => readOrWrite match {
-        case Read => new KijiTap(tableURI, kijiScheme)
+        case Read => new KijiTap(tableURI, kijiScheme).asInstanceOf[Tap[_, _, _]]
         case Write => sys.error("Writing isn't supported currently.")
       }
       case _ => super.createTap(readOrWrite)(mode)
