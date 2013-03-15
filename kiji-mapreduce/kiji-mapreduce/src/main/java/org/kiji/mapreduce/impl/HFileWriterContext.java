3305fef866b7ec6c53350fb42b5a993bdc470137
Christophe Taton <taton@wibidata.com>
2013-03-15 09:25:42 -0800

    SCHEMA-106. CellSpec moved to framework.
    commit: https://github.com/kijiproject/kiji-mapreduce/commit/3f074995a8819377f16b42e8ae26313dfb4df21e

--- kiji-mapreduce/kiji-mapreduce/src/main/java/org/kiji/mapreduce/impl/HFileWriterContext.java
+++ kiji-mapreduce/kiji-mapreduce/src/main/java/org/kiji/mapreduce/impl/HFileWriterContext.java
@@ -39,7 +39,7 @@ import org.kiji.schema.KijiTable;
 import org.kiji.schema.KijiURI;
 import org.kiji.schema.hbase.HBaseColumnName;
 import org.kiji.schema.impl.DefaultKijiCellEncoderFactory;
-import org.kiji.schema.layout.impl.CellSpec;
+import org.kiji.schema.layout.CellSpec;
 import org.kiji.schema.layout.impl.ColumnNameTranslator;
 import org.kiji.schema.util.ResourceUtils;
 
