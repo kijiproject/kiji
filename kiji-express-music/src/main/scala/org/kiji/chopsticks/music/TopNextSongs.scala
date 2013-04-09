9d38d1d40757933ea9b7f795b8574b5a5fb8a323
Kiyan Ahmadizadeh <kiyan@wibidata.com>
2013-04-09 09:56:04 -0800

    CHOP-57. (P5). Adds computing the top next played songs for all songs.
    Reviewed by: robertchu
    commit: https://github.com/kijiproject/kiji-express-music/commit/e9d3ac88807fab5533207eba18ac5b463604db92

---
+++ kiji-express-music/src/main/scala/org/kiji/chopsticks/music/TopNextSongs.scala
@@ -0,0 +1,120 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.chopsticks.music
+
+import scala.collection.JavaConverters._
+
+import com.twitter.scalding._
+
+import com.google.common.collect.Lists
+import org.kiji.chopsticks._
+import org.kiji.chopsticks.DSL._
+import org.kiji.chopsticks.Resources._
+import org.kiji.examples.music.SongCount
+import org.kiji.examples.music.TopSongs
+import org.kiji.schema.EntityId
+import org.kiji.schema.Kiji
+import org.kiji.schema.KijiURI
+
+/**
+ * For each song S, create a list of songs sorted by the number of times a song was played after
+ * S.
+ *
+ * This job accepts two command line arguments, `--users-table` and `--songs-table` that should be
+ * set to the Kiji URIs of a users and songs table in Kiji. The play histories of users (stored
+ * in column `info:track_plays`) are used to compute how many times each song is played after
+ * another. The top next songs for each song are written to the column `info:top_next_songs` of
+ * the songs table.
+ *
+ * @param args passed from the command line.
+ */
+class TopNextSongs(args: Args) extends Job(args) {
+
+  /**
+   * Transforms a Scala `List` into a Java `List`.
+   *
+   * @param ls is the list to transform.
+   * @tparam T is the type of data in the list.
+   * @return a Java `List` created from the original Scala `List`.
+   */
+  def scalaListToJavaList[T](ls: List[T]): java.util.List[T] = Lists.newArrayList[T](ls.asJava)
+
+  /**
+   * Transforms a slice of song ids into a collection of tuples `(s1,
+   * s2)` signifying that `s2` appeared after `s1` in the slice, chronologically.
+   *
+   * @param slice of song ids representing a user's play history.
+   * @return a list of song bigrams.
+   */
+  def bigrams(slice: KijiSlice[String]): List[(String, String)] = {
+    slice.orderChronologically().cells.sliding(2)
+        .map { itr => itr.iterator }
+        .map { itr => (itr.next().datum, itr.next().datum) }
+        .toList
+  }
+
+  /**
+   * Transforms the identifier for a song into an entity id for the songs table.
+   *
+   * @param songId is an identifier for a song.
+   * @return an entity id for the song in the songs table.
+   */
+  def entityId(songId: String): EntityId = {
+    val uri = KijiURI.newBuilder(args("songs-table")).build()
+    retainAnd(Kiji.Factory.open(uri)) { kiji =>
+      retainAnd(kiji.openTable(uri.getTable)) { table =>
+        table.getEntityId(songId)
+      }
+    }
+  }
+
+  /**
+   * Transforms a group of tuples into a group containing a list of song count records,
+   * sorted by count.
+   *
+   * @param nextSongs is the group of tuples containing song count records.
+   * @return a group containing a list of song count records, sorted by count.
+   */
+  def sortNextSongs(nextSongs: GroupBuilder): GroupBuilder = {
+    nextSongs.sortBy('count).reverse.toList[SongCount]('songCount -> 'scalaTopSongs)
+  }
+
+  // This Scalding pipline does the following:
+  // 1. Reads the column "info:track_plays" from a users table in Kiji.
+  // 2. Transforms each user's play history into a collection of bigrams that record when one song
+  //    was played after another.
+  // 3. Counts the number of times each song was played after another.
+  // 4. Creates a song count Avro record from each bigram.
+  // 5. For each song S, creates a list of songs sorted by the number of times the song was played
+  //    after S.
+  // 6. Converts that list from a Scala list to a Java list.
+  // 7. Packs each list into an Avro record.
+  // 8. Creates an entity id for the songs table for each song.
+  // 9. Writes each song's TopSongs record to Kiji.
+  KijiInput(args("users-table"))(Map(Column("info:track_plays", all) -> 'playlist))
+      .flatMap('playlist -> ('firstSong, 'songId)) { bigrams }
+      .groupBy(('firstSong, 'songId)) { _.size('count) }
+      .pack[SongCount](('songId, 'count) -> 'songCount)
+      .groupBy('firstSong) { sortNextSongs }
+      .map('scalaTopSongs -> 'topSongs) { scalaListToJavaList }
+      .pack[TopSongs]('topSongs -> 'topNextSongs)
+      .map('firstSong -> 'entityId) { entityId }
+      .write(KijiOutput(args("songs-table"))('topNextSongs -> "info:top_next_songs"))
+}
---
+++ kiji-express-music/src/test/scala/org/kiji/chopsticks/music/TopNextSongsSuite.scala
@@ -0,0 +1,116 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.chopsticks.music
+
+import scala.collection.mutable.Buffer
+
+import com.twitter.scalding._
+
+import org.kiji.examples.music.TopSongs
+import org.kiji.chopsticks.DSL._
+import org.kiji.chopsticks.{KijiSlice, KijiSuite}
+import org.kiji.schema.EntityId
+
+/**
+ * A test for counting the number of times songs have been played by users.
+ */
+class TopNextSongsSuite extends KijiSuite {
+
+  // Get a Kiji to use for the test and record the Kiji URI of the users and songs tables we'll
+  // test against.
+  val kiji = makeTestKiji("TopNextSongsSuite")
+  val usersURI = kiji.getURI().toString + "/users"
+  val songsURI = kiji.getURI().toString + "/songs"
+
+  // Execute the DDL shell commands in music-schema.ddl to create the tables for the music
+  // tutorial.
+  executeDDLResource(kiji, "org/kiji/chopsticks/music/music-schema.ddl")
+
+  // Create some fake track plays for three users.
+  val testInput =
+      (id("user-0"), slice("info:track_plays", (0L, "song-0"), (1L, "song-1"), (2L, "song-2"))) ::
+      (id("user-1"), slice("info:track_plays", (0L, "song-0"), (1L, "song-0"), (2L, "song-1"))) ::
+      (id("user-2"), slice("info:track_plays", (0L, "song-1"), (1L, "song-2"), (2L, "song-1"))) ::
+      Nil
+
+  /**
+   * Validates the top next songs produces for the three songs used in the test input. The counts
+   * should be as follows.
+   *
+   * Played First     Played Second     Count
+   * song-0           song-0            1
+   * song-0           song-1            2
+   * song-0           song-2            0
+   * song-1           song-0            0
+   * song-1           song-1            0
+   * song-1           song-2            2
+   * song-2           song-0            0
+   * song-2           song-1            1
+   * song-2           song-2            0
+   *
+   * @param topNextSongs contains three tuples for three songs, each containing a record of the
+   *     top next songs played.
+   */
+  def validateTest(topNextSongs: Buffer[(EntityId, KijiSlice[TopSongs])]) {
+    val topSongForEachSong = topNextSongs
+        .map { case(entityId, slice) => (entityId.getComponentByIndex(0).toString,slice) }
+        .map { case(id, slice) => (id, slice.getFirstValue().getTopSongs) }
+
+    topSongForEachSong.foreach {
+      case ("song-0", topSongs) => {
+        assert(2 == topSongs.size())
+        assert("song-1" == topSongs.get(0).getSongId.toString)
+        assert(2 == topSongs.get(0).getCount)
+        assert("song-0" == topSongs.get(1).getSongId.toString)
+        assert(1 == topSongs.get(1).getCount)
+      }
+      case ("song-1", topSongs) => {
+        assert(1 == topSongs.size())
+        assert("song-2" == topSongs.get(0).getSongId.toString)
+        assert(2 == topSongs.get(0).getCount)
+      }
+      case ("song-2", topSongs) => {
+        assert(1 == topSongs.size())
+        assert("song-1" == topSongs.get(0).getSongId.toString)
+        assert(1 == topSongs.get(0).getCount)
+      }
+    }
+  }
+
+  test("TopNextSongs computes how often one song is played after another (local).") {
+    JobTest(new TopNextSongs(_))
+        .arg("users-table", usersURI)
+        .arg("songs-table", songsURI)
+        .source(KijiInput(usersURI)(Map(Column("info:track_plays", all) -> 'playlist)), testInput)
+        .sink(KijiOutput(songsURI)('topNextSongs -> "info:top_next_songs")) { validateTest }
+        .run
+        .finish
+  }
+
+  test("TopNextSongs computes how often one song is played after another (Hadoop).") {
+    JobTest(new TopNextSongs(_))
+        .arg("users-table", usersURI)
+        .arg("songs-table", songsURI)
+        .source(KijiInput(usersURI)(Map(Column("info:track_plays", all) -> 'playlist)), testInput)
+        .sink(KijiOutput(songsURI)('topNextSongs -> "info:top_next_songs")) { validateTest }
+        .runHadoop
+        .finish
+  }
+}
