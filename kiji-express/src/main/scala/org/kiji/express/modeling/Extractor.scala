7ecffa9b48538d5e0ab574d16ce5409b7d32df7d
Robert Chu <robert@wibidata.com>
2013-05-09 14:41:08 -0800

    EXP-19. Adds Extractor trait for Model SPI.
    Reviewed by: kiyan
    commit: https://github.com/kijiproject/kiji-express/commit/dbb6f1ff0bf3f90fcf8ff1ac389776230bdf151a

---
+++ kiji-express/src/main/scala/org/kiji/express/modeling/Extractor.scala
@@ -0,0 +1,126 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.express.modeling
+
+import cascading.tuple.Fields
+import com.twitter.scalding.FieldConversions
+import com.twitter.scalding.TupleConversions
+import com.twitter.scalding.TupleConverter
+import com.twitter.scalding.TupleSetter
+
+import org.kiji.annotations.ApiAudience
+import org.kiji.annotations.ApiStability
+
+/**
+ * Encapsulates both the computation and the data (addressed by field names) required for the
+ * Extract phase of a model workflow
+ *
+ * @param fields required to perform the Extract phase of a model workflow.
+ * @param fn to run when executing the Extract phase of a model workflow.
+ * @tparam I is the type of input data to the Extract phase of this model workflow.
+ * @tparam O is the type of output data coming from Extract phase of this model workflow.
+ */
+@ApiAudience.Public
+@ApiStability.Experimental
+case class ExtractFn[I, O] (
+    fields: (Fields, Fields),
+    fn: I => O)
+
+/**
+ * Represents the Extract phase of a model workflow. Users should extend this trait when defining an
+ * Extract phase. The extraction phase of a model workflow is responsible for transforming raw data
+ * from a Kiji table into feature vector representation, on a per-entity basis.
+ *
+ * For example:
+ * {{{
+ * class MyExtractor extends Extractor {
+ *   override val extractFn = extract('textLine -> 'count) { textLine: KijiSlice[String] =>
+ *     textLine.getFirstValue.split("""\s+""").size
+ *   }
+ * }
+ * }}}
+ *
+ * This trait also provides access to a [[org.kiji.express.modeling.ModelContext]] object that can
+ * be used to access outside data sources required for data extraction.
+ */
+@ApiAudience.Public
+@ApiStability.Experimental
+trait Extractor
+    extends FieldConversions
+    with TupleConversions {
+  /** Container for a model context. This variable should only be set by Pipeline Runners. */
+  private[this] var _context: Option[ModelContext] = None
+
+  /**
+   * Gets the [[org.kiji.express.modeling.ModelContext]] associated with this phase of the model
+   * workflow. This context object can be used to access outside data sources (KVStores) required
+   * for this computation.
+   *
+   * @return the ModelContext associated with this phase of the model workflow.
+   */
+  def context: ModelContext = {
+    _context.getOrElse {
+      throw new IllegalStateException(
+          "This Scorer has not been initialized properly. Its model context hasn't been set.")
+    }
+  }
+
+  /**
+   * Sets the [[ModelContext]] associated with this phase of the model workflow. This should only be
+   * used by KijiExpress Pipeline Runners.
+   *
+   * @param value to set this phase's context object to.
+   */
+  private[express] def context_=(value: ModelContext) {
+    _context = Some(value)
+  }
+
+  /**
+   * Used to define the computation required for the Extract phase of the model workflow.
+   *
+   * @tparam I is the type of input data to the Extract phase of this model workflow.
+   * @tparam O is the type of output data coming from Extract phase of this model workflow.
+   * @return the computation to perform during the Extract phase of the model workflow.
+   */
+  def extractFn: ExtractFn[_, _]
+
+  /**
+   * Used to specify the computation required for the Extract phase of the model workflow along with
+   * the field names of its inputs and the field names to bind to its output.
+   *
+   * @param fields that identify the input to and the output from this phase.
+   * @param fn to run when executing the Extract phase of this model workflow.
+   * @param converter to use when validating the input field name/function argument mapping. This
+   *     argument is provided automatically and users should not need to specify a converter.
+   * @param setter to use when validating the output field name/function return tuple mapping. This
+   *     argument is provided automatically and users should not need to specify a setter.
+   * @tparam I is the type of input data to the Extract phase of this model workflow.
+   * @tparam O is the type of output data coming from Extract phase of this model workflow.
+   */
+  def extract[I, O]
+      (fields: (Fields, Fields))
+      (fn: I => O)
+      (implicit converter: TupleConverter[I], setter: TupleSetter[O]): ExtractFn[I, O] = {
+    converter.assertArityMatches(fields._1)
+    setter.assertArityMatches(fields._2)
+
+    ExtractFn(fields, fn)
+  }
+}
