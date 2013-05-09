bc6159de47c795b994f7e181c77118d2c4d57329
Robert Chu <robert@wibidata.com>
2013-05-09 14:43:49 -0800

    EXP-21. Adds Scorer trait for Model SPI.
    Reviewed by: kiyan
    commit: https://github.com/kijiproject/kiji-express/commit/9d98211444760e1930e2e7556e6a4cdea058d4d4

---
+++ kiji-express/src/test/scala/org/kiji/express/modeling/ScorerSuite.scala
@@ -0,0 +1,101 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.express.modeling
+
+import org.scalatest.FunSuite
+
+class TestScorer extends Scorer {
+  override val scoreFn = score('textLine) { line: String =>
+    line
+        .split("""\s+""")
+        .size
+  }
+}
+
+class TestMalformedScorer extends Scorer {
+  override val scoreFn = score(('field1, 'field2)) { line: String =>
+    line
+  }
+}
+
+class TestMultipleArgumentScorer extends Scorer {
+  override val scoreFn = score(('line1, 'line2)) { lines: (String, String) =>
+    val (line1, line2) = lines
+
+    (line1 + line2)
+        .split("""\s+""")
+        .size
+  }
+}
+
+class ScorerSuite extends FunSuite {
+  test("An scorer can be constructed via reflection") {
+    val actual: Scorer = classOf[TestScorer].newInstance
+    val expected: Scorer = new TestScorer
+
+    val actualFields = actual
+        .scoreFn
+        .fields
+    val expectedFields = expected
+        .scoreFn
+        .fields
+    assert(expectedFields === actualFields)
+
+    val actualFn: (String => Int) = actual
+        .scoreFn
+        .fn
+        .asInstanceOf[(String => Int)]
+    val expectedFn: (String => Int) = expected
+        .scoreFn
+        .fn
+        .asInstanceOf[(String => Int)]
+    assert(expectedFn("foo bar") === actualFn("foo bar"))
+  }
+
+  test("An scorer can have its context object set") {
+    val scorer: Scorer = new TestScorer
+    val thrown = intercept[IllegalStateException] {
+      scorer.context
+    }
+    assert("This Scorer has not been initialized properly. Its model context hasn't been set."
+        === thrown.getMessage)
+
+    val expected: ModelContext = new ModelContext
+    scorer.context = expected
+
+    assert(expected === scorer.context)
+  }
+
+  test("An scorer defined with invalid field selection will fail on construction") {
+    intercept[AssertionError] {
+      new TestMalformedScorer
+    }
+  }
+
+  test("An scorer with multiple arguments") {
+    val scorer: Scorer = new TestMultipleArgumentScorer
+
+    val fn: ((String, String)) => Int = scorer
+        .scoreFn
+        .fn
+        .asInstanceOf[((String, String)) => Int]
+    assert(2 === fn(("hello ", "world")))
+  }
+}
