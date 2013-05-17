42ff7f7c9f95760486812a3ab8f568ba5460dff4
Aaron Feldstein <afeldstein@wibidata.com>
2013-05-17 13:01:07 -0800

    KIJIMR-116. Improve o.k.m.avro package-info.java
    issue: https://jira.kiji.org/browse/KIJIMR-116
    review: https://review.kiji.org/r/487
    commit: https://github.com/kijiproject/kiji-mapreduce/commit/11afbf68ebbc28f9c4f9785201d558dc6ff8ea00

--- kiji-mapreduce/kiji-mapreduce/src/main/java/org/kiji/mapreduce/avro/package-info.java
+++ kiji-mapreduce/kiji-mapreduce/src/main/java/org/kiji/mapreduce/avro/package-info.java
@@ -22,5 +22,23 @@
  *
  * This package is mainly comprised of interfaces that expose avro schemas associated with the
  * input or output of the implementing class.
+ *
+ * <p>
+ *   Analytic functions that write {@link org.apache.avro.mapred.AvroKey} and
+ *   {@link org.apache.avro.mapred.AvroValue} objects should implement
+ *   {@link org.kiji.mapreduce.avro.AvroKeyWriter} and
+ *   {@link org.kiji.mapreduce.avro.AvroValueWriter} respectively. Symmetrically, functions that
+ *   read {@link org.apache.avro.mapred.AvroKey} and {@link org.apache.avro.mapred.AvroValue}
+ *   objects should implement {@link org.kiji.mapreduce.avro.AvroKeyReader} and
+ *   {@link org.kiji.mapreduce.avro.AvroValueReader}.  Commonly this will mean that a Mapper will
+ *   implement AvroKeyReader and AvroValueReader and a Reducer will implement AvroKeyWriter and
+ *   AvroValueWriter.
+ * </p>
+ * <p>
+ *   AvroMapReader is a read-only implementation of java.util.Map (all methods which would normally
+ *   result in a modification to the map will throw UnsupportedOperationException) designed to
+ *   properly compare keys in order to allow use of containsKey() and equals() methods.
+ * </p>
+ *
  */
 package org.kiji.mapreduce.avro;
