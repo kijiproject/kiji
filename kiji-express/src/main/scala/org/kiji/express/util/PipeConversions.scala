470b8a7efc117fe0415b7255826bfa283063a792
Renuka Apte <renuka@wibidata.com>
2013-10-25 13:21:20 -0800

    EXP-234. Implement Association Rules Mining
    issue: https://jira.kiji.org/browse/EXP-234
    review: https://review.kiji.org/r/1249
    commit: https://github.com/kijiproject/kiji-express/commit/b4a08fa51b254fb1a634524883187d84c60a4a3e

--- kiji-express/src/main/scala/org/kiji/express/util/PipeConversions.scala
+++ kiji-express/src/main/scala/org/kiji/express/util/PipeConversions.scala
@@ -29,6 +29,7 @@ import org.kiji.annotations.ApiStability
 import org.kiji.annotations.Inheritance
 import org.kiji.express.flow.KijiPipe
 import org.kiji.express.flow.KijiSource
+import org.kiji.express.modeling.lib.RecommendationPipe
 
 /**
  * PipeConversions contains implicit conversions necessary for KijiExpress that are not included in
@@ -45,7 +46,7 @@ private[express] trait PipeConversions {
    * @param pipe to convert to a KijiPipe.
    * @return a KijiPipe wrapping the specified Pipe.
    */
-  implicit def pipeToKijiPipe(pipe: Pipe): KijiPipe = new KijiPipe(pipe)
+  implicit def pipe2KijiPipe(pipe: Pipe): KijiPipe = new KijiPipe(pipe)
 
   /**
    * Converts a [[org.kiji.express.flow.KijiPipe]] to a [[cascading.pipe.Pipe]].  This
@@ -54,7 +55,7 @@ private[express] trait PipeConversions {
    * @param kijiPipe to convert to [[cascading.pipe.Pipe]].
    * @return Pipe instance wrapped by the input KijiPipe.
    */
-  implicit def kijiPipeToPipe(kijiPipe: KijiPipe): Pipe = kijiPipe.pipe
+  implicit def kijiPipe2Pipe(kijiPipe: KijiPipe): Pipe = kijiPipe.pipe
 
   /**
    * Converts a [[org.kiji.express.flow.KijiPipe]] to a [[com.twitter.scalding.RichPipe]].  This
@@ -62,7 +63,7 @@ private[express] trait PipeConversions {
    * @param kijiPipe to convert to [[com.twitter.scalding.RichPipe]].
    * @return RichPipe instance of Pipe wrapped by input KijiPipe.
    */
-  implicit def kijiPipeToRichPipe(kijiPipe: KijiPipe): RichPipe = new RichPipe(kijiPipe.pipe)
+  implicit def kijiPipe2RichPipe(kijiPipe: KijiPipe): RichPipe = new RichPipe(kijiPipe.pipe)
 
   /**
    * Converts a KijiSource to a KijiExpress KijiPipe. This method permits implicit conversions
@@ -74,8 +75,33 @@ private[express] trait PipeConversions {
    * @param source to convert to a KijiPipe
    * @return a KijiPipe read from the specified source.
    */
-  implicit def source2rp(
+  implicit def source2RichPipe(
       source: KijiSource)(
       implicit flowDef: FlowDef,
       mode: Mode): KijiPipe = new KijiPipe(source.read(flowDef, mode))
+
+  /**
+   * Converts a Cascading Pipe to an Express Recommendation Pipe. This method permits implicit
+   * conversions from Pipe to RecommendationPipe.
+   *
+   * @param pipe to convert to a RecommendationPipe.
+   * @return a RecommendationPipe wrapping the specified Pipe.
+   */
+  implicit def pipe2RecommendationPipe(pipe: Pipe): RecommendationPipe =
+    new RecommendationPipe(pipe)
+
+  /**
+   * Converts a KijiSource to a KijiExpress Recommendation Pipe. This method permits implicit
+   * conversions from Source to RecommendationPipe.
+   *
+   * We expect flowDef and mode implicits to be in scope.  This should be true in the context of a
+   * Job, KijiJob, or inside the ShellRunner.
+   *
+   * @param source to convert to a KijiPipe.
+   * @return a RecommendationPipe read from the specified source.
+   */
+  implicit def source2RecommendationPipe(
+      source: KijiSource)(
+      implicit flowDef: FlowDef,
+      mode: Mode): RecommendationPipe = new RecommendationPipe(source.read(flowDef, mode))
 }
