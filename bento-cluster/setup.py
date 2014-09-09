cbe9451bd778b3d9ea322f0c35b0649c226060e6
Robert Chu <robert@wibidata.com>
2014-09-08 22:16:46 -0800

    CLUSTER-81. Update pip package version to 2.0.4.
    issue: https://jira.kiji.org/browse/CLUSTER-81
    review: https://review.kiji.org/r/2252
    over-the-shoulder-reviewers: afeldstein
    commit: https://github.com/kijiproject/bento-cluster/commit/883088641f40b3997956e60f1092f95d78bd1015

--- bento-cluster/setup.py
+++ bento-cluster/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 def main(args):
     setup(
         name="kiji-bento-cluster",
-        version="2.0.3",
+        version="2.0.4",
 
         # Lists the Python modules provided by this package:
         packages=[
