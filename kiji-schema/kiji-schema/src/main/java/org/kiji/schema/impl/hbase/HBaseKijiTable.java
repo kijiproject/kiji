b4db6207ae51e8a7e1b2684bcd92ddeaf7e24f9b
Robert Chu <robert@wibidata.com>
2014-11-25 15:11:51 -0800

    WIBI-1910. Fix checkstyle issue in kiji-schema due to unused import.
    issue: https://jira.wibidata.com/browse/WIBI-1910
    review: https://review.wibidata.net/r/3742

--- kiji-schema/kiji-schema/src/main/java/org/kiji/schema/impl/hbase/HBaseKijiTable.java
+++ kiji-schema/kiji-schema/src/main/java/org/kiji/schema/impl/hbase/HBaseKijiTable.java
@@ -29,7 +29,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HRegionLocation;
