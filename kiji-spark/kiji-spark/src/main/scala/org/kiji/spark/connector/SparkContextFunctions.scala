08a78a1f8f3a0623c7f5c0574f074499351518c1
Adam Kunicki <adam@wibidata.com>
2015-01-15 16:57:59 -0800

    SPARK-49. Add support for cluster mode on secure clusters
    issue: https://jira.wibidata.com/browse/SPARK-49
    review: https://review.wibidata.net/r/4115

--- kiji-spark/kiji-spark/src/main/scala/org/kiji/spark/connector/SparkContextFunctions.scala
+++ kiji-spark/kiji-spark/src/main/scala/org/kiji/spark/connector/SparkContextFunctions.scala
@@ -18,6 +18,7 @@
  */
 package org.kiji.spark.connector
 
+import org.apache.hadoop.hbase.security.token.TokenUtil
 import org.apache.hadoop.security.UserGroupInformation
 import org.apache.spark.SparkContext
 import org.kiji.schema.KijiDataRequest
@@ -28,6 +29,8 @@ import org.slf4j.LoggerFactory
 /** Provides Kiji-specific methods on `SparkContext` */
 class SparkContextFunctions(@transient val sc: SparkContext) extends Serializable {
 
+  import SparkContextFunctions._
+
   /** Returns a view of a Kiji table as `KijiRDD[T]`.
     * This method is made available on `SparkContext` by importing `org.kiji.spark._`
     *
@@ -37,7 +40,34 @@ class SparkContextFunctions(@transient val sc: SparkContext) extends Serializabl
     * @return An instance of a KijiRDD.
     */
   def kijiRDD[T](uri: KijiURI, dataRequest: KijiDataRequest, vClass: Class[_ <: T]): KijiRDD[T] = {
-    val ugi = UserGroupInformation.getCurrentUser
+    val authMode = sc.hadoopConfiguration.get("hbase.security.authentication")
+    Log.info(s"Running with $authMode authentication.")
+
+    UserGroupInformation.setConfiguration(sc.hadoopConfiguration)
+
+    val sparkConf = sc.getConf
+
+    val kerberosUsername = sparkConf.getOption("spark.kiji.kerberos.username")
+    val keytab = sparkConf.getOption("spark.kiji.kerberos.keytab")
+
+    // If the user specified both properties, then attempt to authenticate
+    val ugi = if (kerberosUsername.nonEmpty && keytab.nonEmpty) {
+      val ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(
+        kerberosUsername.get,
+        keytab.get
+      )
+      // Even if we authenticated, only request a token if security is enabled.
+      if (UserGroupInformation.isSecurityEnabled) {
+        TokenUtil.obtainAndCacheToken(sc.hadoopConfiguration, ugi)
+        Log.info("Obtained and cached auth token for HBase.")
+      }
+      ugi
+    } else {
+      // Otherwise assume we are either on a non-secure cluster or the HBase auth token
+      // has already been cached by the user.
+      UserGroupInformation.getCurrentUser
+    }
+
     val credentials = ugi.getCredentials
     KijiRDD(sc, sc.hadoopConfiguration, credentials, uri, dataRequest).asInstanceOf[KijiRDD[T]]
   }
