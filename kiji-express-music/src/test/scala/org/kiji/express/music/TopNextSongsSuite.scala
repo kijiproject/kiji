a96f2c2a9498e3359a41d38220073cf262e2b4b7
Kevin Laube <kevin@wibidata.com>
2015-01-20 17:29:30 -0800

    WIBIEXP-434. Fix scalastyle regression in kiji-express-music
    issue: https://jira.wibidata.com/browse/WIBIEXP-434
    review: https://review.wibidata.net/r/4170

--- kiji-express-music/src/test/scala/org/kiji/express/music/TopNextSongsSuite.scala
+++ kiji-express-music/src/test/scala/org/kiji/express/music/TopNextSongsSuite.scala
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.kiji.express.music
 
 import scala.collection.mutable.Buffer
